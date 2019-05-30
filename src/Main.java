162324bc636b777e18b1abcab4bca54f4ebc0fa9
Fateh Boucenna <boucenna.fateh@gmail.com>
2019-05-30 12:20:24 +0100

    added grouping technique

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int dataCollectionSize = 1000;
-        int accessesNbr = 100000;
-        String docFile = "doc.dat";
-        String versionFile = "version.dat";
-        final int BLOCS_NBR = 10 ;
-        final int VERSIONS_NBR = 10;
-        final boolean isDummy = false;
-        final boolean isGrouping = true;
+        // you can change this parameters
+
+        int dataCollectionSize = 1000; // the size of the data collection
+        int accessesNbr = 100000; // the number of accesses to the data collection
+        String docFile = "doc.dat"; // result when the approach is not applied
+        String versionFile = "version.dat"; // result when the approach is applied
+        final int BLOCS_NBR = 10 ; // number of blocks for a document
+        final int VERSIONS_NBR = 10; // number of versions of a block
+        boolean splitting = true; // separating + splitting techniques are applied
+        boolean scrambling = true; // separating + splitting + scrambling techniques are applied
+        boolean grouping = true; // separating + splitting + scrambling + grouping techniques are applied
+
+
+        boolean isDummy = false; // don't change
+        boolean isGrouping = false; // don't change
+
+        if (splitting){
+            isDummy = false;
+            isGrouping = false;
+        } else if (scrambling){
+            isDummy = true;
+            isGrouping = false;
+        } else if (grouping) {
+            isDummy = false;
+            isGrouping = true;
+        }
+
         final int dummy_per_document = 5;
 
         DocumentsManagement documentsManagement = new DocumentsManagement(dataCollectionSize, BLOCS_NBR, VERSIONS_NBR, dummy_per_document, isDummy, isGrouping);
